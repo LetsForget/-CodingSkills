c821e12b5f492855127d98d5a3370c6fdfd0cf1e
Mikhail Kryloff <crashanya2@yandex.ru>
2018-09-11 11:34:50 +0500

    Another try to get normally working git

---
+++ repos/First/First/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] diff = input.Split(new char[] { ' ' });
+            int a;           
+            var nums = from x in diff
+                       where int.TryParse(x, out a)
+                       select x;
+            for (int i = nums.Count() - 1; i > -1 ; i--)
+                Console.Write(nums.ToList()[i] + " ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ repos/Second/Second/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int shift = int.Parse(input);
+
+            input = Console.ReadLine();
+            string[] div = input.Split(new char[] { ' ' });
+            string[] result = new string[div.Length];
+
+            for (int i = 0; i < result.Length; i++)
+                result[i] = div[(i + shift) % div.Length];
+
+            Console.Write(string.Join(" ", result));
+            Console.ReadKey();
+        }
+    }
+}
